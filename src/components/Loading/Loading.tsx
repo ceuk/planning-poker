196178dce7fe1f4c2b56963b65d760befb054322
ceuk <1681236+ceuk@users.noreply.github.com>
2022-04-27 22:37:04 +0100

    loader styles

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,13 @@
+import styles from './Loading.module.scss'
+
 const Loading = () => (
-  <div>Loading...</div>
+  <div className={styles.root}>
+    <div className={styles.card}>
+      <div className={styles.cardFront}>5</div>
+      <div className={styles.cardBack} />
+    </div>
+    <h1 className={styles.heading}>Connecting&hellip;</h1>
+  </div>
 )
 
 export default Loading
